ed979c07ab808c02891e639b56acc1a7abc8ee68
Oleg Korkeshko <rik.san4ez2018@yandex.ru>
2018-04-04 21:15:56 +0500

    demo

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 import requests
-from bs4 import BeautifulSoup as bs
+#from bs4 import BeautifulSoup as bs
 import re 
 import pickle
 from models import Title
-
+import asyncio
 def get_last_episode(url):
 	try:
 		html=requests.get(url)
@@ -20,6 +20,7 @@ def get_or_create_server_file(server,channel):
 	except FileNotFoundError:
 		with open(server,'wb') as f:
 			pickle.dump((channel,[]),f)
+	return server
 
 	return server
 def save_user_link_episode(user, url, episode,file):
@@ -35,4 +36,28 @@ def save_user_link_episode(user, url, episode,file):
 	with open(file,'wb') as f:
 		pickle.dump((channel,titles),f)
 
+async def check_new_eps(bot):
+	await bot.wait_until_ready()
+	servers = list(bot.servers)
+	while not bot.is_closed:
+		for server in servers:
+			server_name=str(server)+'.pickle'
+			try:
+				with open(server_name,'rb') as f:
+					channel,titles = pickle.load(f)
+			except FileNotFoundError:
+				continue
+			for title in titles:
+				last_episode = get_last_episode(title.url)
+				if last_episode == "404" or last_episode == "ConnectionError":	
+					await bot.send_message(channel, "Ошибка при обработке - "+title.url)
+					continue
+				if title.last_episode < last_episode:
+					message = ', '.join(title.users) + ' Вышли новые серии!\n' + title.url
+					await bot.send_message(channel, message)
+					title.last_episode = last_episode
+			with open(str(server)+'.pickle','wb') as f:
+				pickle.dump((channel,titles), f)
+				await asyncio.sleep(3)
+		await asyncio.sleep(3600)
 
--- good_bot.py
+++ good_bot.py
@@ -1,5 +1,6 @@
 from functions import (
-					get_last_episode, 
+					get_last_episode,
+					check_new_eps, 
 					save_user_link_episode,
 					get_or_create_server_file)
 from discord.ext import commands
@@ -7,9 +8,14 @@ import discord
 import pickle
 import asyncio
 
-token='NDE3Njk2Nzc4OTkzNzk1MDcy.DaT9iw.ZnnKcAyFm16PZ6g_vxHxrnNu2ZI'
+description=(
+	"""
 
-bot=commands.Bot(command_prefix='?')
+	Бот. Напоминает. О выходе новых серий. Но это не точно.
+
+	""")
+token='NDE3Njk2Nzc4OTkzNzk1MDcy.DaZ1zA.BryXV3N31iH02Knk_JMUchGi4Dw'
+bot=commands.Bot(command_prefix='?',description=description)
 
 @bot.event
 async def on_ready():
@@ -18,48 +24,25 @@ async def on_ready():
 	print(bot.user.id)
 	print('-'*18)
 
-async def check_new_eps():
-	await bot.wait_until_ready()
-	servers = list(bot.servers)
-	while not bot.is_closed:
-		for server in servers:
-			server_name=str(server)+'pickle'
-			try:
-				with open(server_name,'rb') as f:
-					channel,titles = pickle.load(f)
-			except FileNotFoundError:
-				continue
-			for title in titles:
-				last_episode = get_last_episode(title.url)
-				if last_episode == "404" or last_episode == "ConnectionError":	
-					last_episode = '0'
-					await bot.send_message(channel, "Ошибка при обработке - "+title.url)
-				if title.last_episode < last_episode:
-					message = ', '.join(title.users) + ' Вышли новые серии!\n' + title.url
-					await bot.send_message(channel, message)
-					title.last_episode = last_episode
-			with open(str(server)+'pickle','wb') as f:
-				pickle.dump(titles, f)
-			await asyncio.sleep(3)
-		await asyncio.sleep(3600)
-
-@bot.event
-async def on_message(message):
-
-	if message.content.startswith('?save'):
-		server_file = get_or_create_server_file(message.server,message.channel)
-		try:
-			_, url= message.content.split(' ')
-			last = get_last_episode(url)
+@bot.command(pass_context=True)
+async def save(ctx, url:str = ''):
+	"""добавить сериал в список напоминаний - ?save <url>"""
+	if url =='':
+		await bot.say("url не найден!")
+		return
+	server_file = get_or_create_server_file(
+			ctx.message.server,ctx.message.channel)
+	last = get_last_episode(url)
 			
-			if last == "404" or last == "ConnectionError":
-				await bot.send_message(message.channel,'Ошибка - не рабочий url')
-			else:
-				save_user_link_episode(str(message.raw_mentions),url,last,server_file)
-				await bot.send_message(message.channel,'Последни эпизод - '+last)
-		except ValueError:
-			await bot.send_message(message.channel,'Ошибка - больше одного аргумента (?save <url>)')		
-	await bot.process_commands(message)
+	if last == "404" or last == "ConnectionError":
+		await bot.say('Ошибка - не рабочий url')
+	else:
+		save_user_link_episode(str(ctx.message.author.mention),
+			url,last,server_file)
+		await bot.send_message(ctx.message.channel,
+			'Последни эпизод - '+last)
 
-bot.loop.create_task(check_new_eps())
+
+
+bot.loop.create_task(check_new_eps(bot))
 bot.run(token)
